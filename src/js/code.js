0f5403119d78c9b2c7f127b74b0594ce3703b2df
alpharooq <alpharooq.ninja@gmail.com>
2017-03-11 02:36:07 +0200

    V1.0

---
+++ src/js/code.js
@@ -0,0 +1,20 @@
+var box_home = $('.box_home').height();
+var box_home_content = $('.box_home .content').height();
+$('.box_home').css({
+    'padding-top' : ((box_home-box_home_content)/2)
+});
+$(document).ready(function() {
+
+    $(".list").pinBox({
+        Top : '50px',
+        Container : '#pinBoxContainer',
+    });
+
+});     
+$(document).on('click','a', function(event) {
+    event.preventDefault();
+    var target = "#" + this.getAttribute('godiv');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 500);
+});
